b636bb6aa89a68c732f8db20b64ca1a16961ea0a
James Harton <james@automat.nz>
2019-10-07 12:57:38 +1300

    Add post-hoc connect and disconnect functions.

--- lib/ssd1306.ex
+++ lib/ssd1306.ex
@@ -13,4 +13,20 @@ defmodule SSD1306 do
   Then use the functions in [SSD1306.Device] to send image data.
   Pretty simple.
   """
+
+  @doc """
+  Connect to an SSD1306 device.
+  """
+  def connect(config),
+    do:
+      Supervisor.start_child(SSD1306.Supervisor, %{
+        id: {SSD1306.Device, Map.fetch!(config, :bus), Map.fetch!(config, :address)},
+        start: {SSD1306.Device, :start_link, [config]}
+      })
+
+  @doc """
+        Disconnect an SSD1306 device.
+  """
+  def disconnect(device_name),
+    do: Process.exit({:via, Registry, {SSD1306.Registry, device_name}}, :normal)
 end
